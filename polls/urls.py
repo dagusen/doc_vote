081f2ba8a4dc7f09462b28cd195b41d01ff7e464
MD Tareq Mahmud <tareqmahmud9@gmail.com>
2017-07-16 16:25:06 +0600

    Complete Details Feature

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from .views import index
+from .views import index, detail
 
 urlpatterns = [
     # /polls
     url(r'^$', index, name="index"),
+
+    # /polls/poll_id
+    url(r'^(?P<poll_id>[0-9]+)$', detail, name="detail"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.http import Http404
 from polls.models import Question, Choice
 
 
 def index(request):
     polls = Question.objects.all()
     return render(request, "polls/index.html", {"polls": polls})
+
+
+def detail(request, poll_id):
+    try:
+        poll = Question.objects.get(pk=poll_id)
+    except Question.DoesNotExist:
+        raise Http404("Sorry Poll Doesn't Found")
+
+    return render(request, "polls/detail.html", {"poll": poll})
